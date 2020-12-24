9c538ba8eb50815c82c53ecb63a82fae5dbf7e8f
test-pr <moiseychenko@gmail.com>
2020-12-24 03:43:55 +0300

    [READY] moiseychenko lab6

---
+++ moiseychenko/lab6/src/distribution.asm
@@ -0,0 +1,42 @@
+.model flat,c
+
+.code
+distribution proc C arr: dword, LGrInt: dword, NumRanDat: dword, result: dword
+
+	
+	mov ebx, [arr]
+	mov esi, [LGrInt]
+	mov edi, [result]
+	mov ecx, [NumRanDat]
+
+	my_loop:
+		mov eax, [ebx] ; eax = current_arr
+		add ebx, 4 ; ebx = next_arr
+		cmp eax, [esi]
+		jl loop_end ; if current_arr < current_LGrInt
+		
+		; current_arr >= current_LGrInt
+	comparison:
+		cmp eax, [esi+4] ; if current_arr > next_LGrInt
+		jg next_LGrInt
+
+		; if current_arr <= next_LGrInt: current_result++
+		mov eax, [edi]
+		add eax, 1
+		mov [edi], eax
+		jmp loop_end
+
+	next_LGrInt:
+		add esi, 4 ; esi = next_LGrInt
+		add edi, 4 ; edi = next_result
+		jmp comparison ; repeat comparison with the next LGrInt
+
+	loop_end:
+		dec ecx
+		cmp ecx, 0
+		jne my_loop
+
+	ret
+
+distribution endp
+end
---
+++ moiseychenko/lab6/src/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <algorithm>
+#include <ctime>
+using namespace std;
+
+
+extern "C" void distribution(int* arr, int* LGrInt, int NumRanDat, int* result);
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int NumRanDat = 0, Xmin = 0, Xmax = 0, NInt = 0;
+	cout << "Enter the size of the array (<=2^14):\n";
+	cin >> NumRanDat;
+	if (NumRanDat > 16 * 1024 || NumRanDat <= 0) {
+		cout << "error: wrong array size entered\n";
+		exit(1);
+	}
+	cout << "Enter the lower limit:\n";
+	cin >> Xmin;
+	cout << "Enter the upper limit:\n";
+	cin >> Xmax;
+	if (Xmin > Xmax) {
+		cout << "error: wrong limits entered\n";
+		exit(1);
+	}
+	cout << "Enter the number of ranges (<= 24): \n";
+	cin >> NInt;
+	if (NInt > 24 || NInt < 1 || NInt >(Xmax - Xmin + 1)) {
+		cout << "error: wrong number of ranges entered\n";
+		exit(1);
+	}
+	int* LGrInt = new int[NInt+1]();
+	LGrInt[0] = Xmin;
+	LGrInt[NInt] = Xmax + 1;
+	if (NInt == 2)
+		cout << "Enter the lower limit of the second range:\n";
+	else
+		cout << "Enter " << NInt - 1 << " lower range limits:\n";
+	for (int i = 1; i < NInt; i++) {
+		cin >> LGrInt[i];
+		while (LGrInt[i] < LGrInt[i-1]) {
+			cout << "The limit should be greater than the previous one (>" << LGrInt[i-1] << ")\n";
+			cin >> LGrInt[i];
+		}
+		if (LGrInt[i] < Xmin || LGrInt[i] > Xmax) {
+			cout << "error: wrong lower limit entered\n";
+			exit(1);
+		}
+	}
+	int* arr = new int[NumRanDat]();
+	for (int i = 0; i < NumRanDat; i++) {
+		arr[i] = rand() % (Xmax - Xmin + 1) + Xmin;
+	}
+	sort(arr, arr + NumRanDat);
+	int* result = new int[NInt];
+	for (int i = 0; i < NInt; i++)
+		result[i] = 0;
+	distribution(arr, LGrInt, NumRanDat, result);
+	LGrInt[NInt] -= 1;
+	ofstream file("result.txt");
+	cout << "\nGenerated numbers:\n";
+	file << "Generated numbers:\n";
+	for (int i = 0; i < NumRanDat; i++) {
+		cout << arr[i] << " ";
+		file << arr[i] << " ";
+	}
+	cout << "\n\n";
+	file << "\n\n";
+	cout << "Number | Range | Quantity\n";
+	file << "Number | Range | Quantity\n";
+	cout << "-------------------------" << endl;
+	file << "-------------------------" << endl;
+	for (int i = 0; i < NInt; i++) {
+		file << "  " << i + 1 << "  |  " << LGrInt[i] << ", " << LGrInt[i + 1] << "  |  " << result[i] << "\n";
+		cout << "  " << i + 1 << "  |  " << LGrInt[i] << ", " << LGrInt[i + 1] << "  |  " << result[i] << "\n";
+	}
+}
